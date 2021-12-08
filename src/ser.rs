7f53408109a27db5a4ba372bd9f8eef79ad649ac
Guy Korland <gkorland@gmail.com>
2021-12-08 08:49:28 +0200

    Clippy warnings (#1)
    * clean pedantic warnings

--- src/ser.rs
+++ src/ser.rs
@@ -148,7 +148,7 @@ impl Serializer for ValueSerializer {
 
     #[inline]
     fn serialize_char(self, value: char) -> Result<IValue, Self::Error> {
-        let mut buffer = [0u8; 4];
+        let mut buffer = [0_u8; 4];
         Ok(value.encode_utf8(&mut buffer).into())
     }
 
@@ -487,7 +487,7 @@ impl Serializer for ObjectKeySerializer {
 
     #[inline]
     fn serialize_char(self, value: char) -> Result<IString, Self::Error> {
-        let mut buffer = [0u8; 4];
+        let mut buffer = [0_u8; 4];
         Ok(value.encode_utf8(&mut buffer).into())
     }
 
