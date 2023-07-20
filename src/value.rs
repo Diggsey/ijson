26bf896c5ff6ab9a8251a76f0fb9faafc8f1a014
ThisAccountHasBeenSuspended <139617589+ThisAccountHasBeenSuspended@users.noreply.github.com>
2023-07-20 14:34:27 +0200

    Don't duplicate the existing conversions

--- src/value.rs
+++ src/value.rs
@@ -946,7 +946,6 @@ impl From<bool> for IValue {
     }
 }
 
-#[cfg(not(feature = "indexmap"))]
 typed_conversions! {
     INumber: i8, u8, i16, u16, i32, u32, i64, u64, isize, usize;
     IString: String, &String, &mut String, &str, &mut str;
@@ -960,14 +959,7 @@ typed_conversions! {
 
 #[cfg(feature = "indexmap")]
 typed_conversions! {
-    INumber: i8, u8, i16, u16, i32, u32, i64, u64, isize, usize;
-    IString: String, &String, &mut String, &str, &mut str;
-    IArray:
-        Vec<T> where (T: Into<IValue>),
-        &[T] where (T: Into<IValue> + Clone);
     IObject:
-        HashMap<K, V> where (K: Into<IString>, V: Into<IValue>),
-        BTreeMap<K, V> where (K: Into<IString>, V: Into<IValue>),
         IndexMap<K, V> where (K: Into<IString>, V: Into<IValue>);
 }
 
